26fd2a80445d30000870fd6ef6218ab498442405
z5258927 <z5258927@vx07>
2023-06-04 10:42:14 +1000

    week 1 tutorial code

---
+++ wk01/hello_win.c
@@ -0,0 +1,16 @@
+// Author: Jenny King (z55555555)
+// May 2023
+// Very basic hello world program
+
+// includes the standard i/o library
+// allows us to use printf, and  (later) scanf etc.
+#include <stdio.h>
+
+int main(void) {
+    // prints output to the command line
+    printf("Hello world\n");
+    printf(":))))\n");
+
+    return 0;
+}
+
